dd79e4dee122902eca7aab7f3829477e7124e6ef
Hugo Cesar <hugocesar.fm@gmail.com>
2024-10-27 14:16:14 -0300

    Telas de cadastro, estruturas dos ponteiros e dados

---
+++ structs_ponteiros.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <conio.h>
+#include <time.h>
+
+// Estrutura typedef
+typedef struct
+{
+    int cd_conta;
+    char banco[50];
+    char agencia[10];
+    char numero_conta[20];
+    char tipo_conta[20];
+    double vl_saldo;
+    double vl_limite;
+    char status[10];
+} conta_bancaria;
+
+// Movimentacao bancaria
+typedef struct
+{
+    int sequencial;
+    int cd_conta;
+    char dt_movimento[11];
+    char tp_movimentacao[15];
+    double vl_movimento;
+    double vl_limite;
+    char status[10];
+} movimenta_bancaria;
+
+// Ponteiro
+typedef struct ContaBancaria *TipoApontador;
+
+// Estrutura para armazenar o conteudo
+typedef struct ContaBancaria
+{
+    conta_bancaria conteudo;
+    TipoApontador proximo;
+} ContaBancaria;
+
+// Estrutura para aterrar o ponteiro
+typedef struct
+{
+    TipoApontador Primeiro;
+    TipoApontador Ultimo;
+} ContaBancaria;
